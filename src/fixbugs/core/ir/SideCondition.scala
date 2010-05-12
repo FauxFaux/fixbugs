c378be46dc460130779e2a4dc52033ea2f215665
Richard Warburton <richard@nomad>
2010-05-12 17:14:23 +0100

    adds licensing to files

--- src/fixbugs/core/ir/SideCondition.scala
+++ src/fixbugs/core/ir/SideCondition.scala
@@ -1,3 +1,23 @@
+
+/**
+ *
+ * This file is part of Fixbugs.
+ * 
+ * Fixbugs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Fixbugs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Fixbugs.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
 package fixbugs.core.ir
 
 sealed abstract class SideCondition {}
